3c0f1a845e476dc3d48cdaf3a29ab565b6d90c0c
Ash4059 <jaiswalashish405@gmail.com>
2025-03-19 22:13:41 +0530

    More than one constructor with different parameter

--- src/main/java/Learning/TravelPlanner/Service/Implementation/ActivityImplementation.java
+++ src/main/java/Learning/TravelPlanner/Service/Implementation/ActivityImplementation.java
@@ -2,6 +2,9 @@ package Learning.TravelPlanner.Service.Implementation;
 
 import java.util.*;
 
+import Learning.TravelPlanner.Entity.Ticket;
+import Learning.TravelPlanner.Entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import Learning.TravelPlanner.Entity.Activity;
@@ -11,6 +14,23 @@ import Learning.TravelPlanner.Service.ActivityService;
 public class ActivityImplementation implements ActivityService {
 
     List<Activity> activities;
+//    Spring create object using empty parameter contructor if present. If two constructor are present with
+//    different parameter then whatever bean is called we need to add @Autowired to the constructor
+//    User user;
+//    Ticket ticket;
+
+//    @Autowired
+//    public ActivityImplementation(User user){
+//        this.user = user;
+//        activities = new ArrayList<>();
+//        System.out.println("User object is initialized");
+//    }
+//
+//    public ActivityImplementation(Ticket ticket){
+//        this.ticket = ticket;
+//        activities = new ArrayList<>();
+//        System.out.println("Ticket object is initialized");
+//    }
 
     public ActivityImplementation(){
         activities = new ArrayList<>();
